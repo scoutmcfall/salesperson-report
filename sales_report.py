8a49e123a73490f40177228c75c25fcd6e2d5748
Scout McFall <scoutmknz@gmail.com>
2021-09-09 16:43:52 +0000

    clean up test statements

--- sales_report.py
+++ sales_report.py
@@ -1,9 +1,4 @@
 """Generate sales report showing total melons each salesperson sold."""
-
-#make empty lists for salespeople and melons sold
-salespeople = []
-melons_sold = []
-
 #--------------------
 #open the sales report file
 f = open('sales-report.txt')
@@ -15,30 +10,24 @@ for line in f:#for each line in the file, split it up by |
     entries = line.split('|')#now we have a list called entries for each line in the text file
     
     for i in range(len(entries)-2):#populate the dictionary with names as keys
-            # attributes_list= [entries[1], entries[2])#will need a solution if attributes added later
-            #     for elem in attributes_list:
-            #         key = "cost"
-            #         attributes_dict[key] = elem[0]
-            #value = (entries[-1:1])#makes this flexible by adding everything from the end to the second entry
-            key= entries[0]
-            #value = []
-            #melon_dict[key] = value
-            melon_dict[key] = melon_dict.get(key, []) 
-            melon_dict[key].append(float(entries[1]))
-            melon_dict[key].append(int(entries[2]))
-            #print("this is the current value", melon_dict[key])
-#print("test, line 26", melon_dict)
+        key= entries[0]
+        melon_dict[key] = melon_dict.get(key, []) 
+        melon_dict[key].append(float(entries[1]))
+        melon_dict[key].append(int(entries[2]))
+       
 for name, value in melon_dict.items():
     total_cost = sum(value[0::2])
     total_count = sum(value[1::2])
     print(name.upper()) #prints name in uppercase
     print(f'sold ${total_cost}: {total_count} melons')
-    #print(value)
     print("====================")
 
 
 #-------------------------------
-#     salesperson = entries[0]#assign the 0th item to salesperson
+#make empty lists for salespeople and melons sold
+# salespeople = []
+# melons_sold = []    
+# salesperson = entries[0]#assign the 0th item to salesperson
 #     melons = int(entries[2])#assign the int value of the 2nd item to melons
 
 #     if salesperson in salespeople:#if the salesperson is already in salespeople list
